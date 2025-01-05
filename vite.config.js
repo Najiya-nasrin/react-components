01e207f61a0fbe0577b199bddf1be6c8cda3fab9
Najiya Nasrin <mails4najiya@gmail.com>
2025-01-06 00:28:45 +0400

    check

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-components",
-  plugins: [react()],
-  build: {
-    outDir: 'build',
-  }
+  plugins: [react()]
 })
